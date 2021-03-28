e6dd48acf438b17db2ce3aad051ad256dd77dd00
alots <https://github.com/alots>
2021-03-29 00:36:17 +0500

    dynamics has been added to the component

--- src/components/ForecastComponent.js
+++ src/components/ForecastComponent.js
@@ -1,56 +1,78 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
 
 import "./style.scss";
 
+const R_CIRCLE = "116";
+const R_INNER_CIRCLE = "84";
 
+const circumFerence = (radius) => {
+  return 2*3.14*radius
+};
 
 const ForecastComponent = () => {
+  const [percent, setPercent] = useState(95);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    setPercent(Number(data.percent));
+    console.log(typeof(data.percent));
+  };
+
+  useEffect(() => {
+    const circle = document.querySelector('.progress-ring__circle');
+    const circleInner = document.querySelector('.progress-ring__circle-orange');
+    const radius = circle.r.baseVal.value;
+    const radiusInner = circleInner.r.baseVal.value;
+    circle.style.strokeDashoffset = `${circumFerence(radius) - circumFerence(radius)/100*percent}`;
+    circleInner.style.strokeDashoffset = `${circumFerence(radiusInner) - circumFerence(radiusInner)/100*percent}`;
+  }, [percent]);
+  
   return (
     <>
     <div className="container"> 
       <svg
-        class="progress-ring"
+        className="progress-ring"
         height="240"
         width="240"
       >
         <circle
-          class="progress-ring__circle-grey"
+          className="progress-ring__circle-grey"
           stroke="rgba(180, 184, 183, 0.5)"
-          stroke-width="4"
+          strokeWidth="4"
           fill="transparent"
-          r="116"
+          r={R_CIRCLE}
           cx="120"
           cy="120"
         />
         <circle
-          class="progress-ring__circle"
+          className="progress-ring__circle"
           stroke="grey"
-          stroke-width="4"
+          strokeWidth="4"
           fill="transparent"
-          r="116"
+          r={R_CIRCLE}
           cx="120"
           cy="120"
         />
         <svg
-          class="progress-ring"
+          className="progress-ring"
           height="240"
           width="240"
         >
           <circle
-            class="progress-ring__circle-grey"
+            className="progress-ring__circle-grey"
             stroke="rgba(180, 184, 183, 0.5)"
-            stroke-width="36"
+            strokeWidth="36"
             fill="transparent"
-            r="84"
+            r={R_INNER_CIRCLE}
             cx="120"
             cy="120"
           />
           <circle
-            class="progress-ring__circle-orange"
+            className="progress-ring__circle-orange"
             stroke="rgba(245, 112, 51, 0.89)"
-            stroke-width="36"
+            strokeWidth="36"
             fill="transparent"
-            r="84"
+            r={R_INNER_CIRCLE}
             cx="120"
             cy="120"
           />   
@@ -58,7 +80,7 @@ const ForecastComponent = () => {
       </svg>
       <div className="card">
         <div className="card__progress-bar-value"> 
-          <font size="48">95</font>
+          <font size="48">{ percent }</font>
           <font size="4">%</font>
         </div>
         <div className="card__item">
@@ -76,8 +98,14 @@ const ForecastComponent = () => {
           </div>
         </div>
       </div>
-      <div className="progress-next-percent">96%</div>
+      <div className="progress-next-percent">{percent + 1}%</div>
     </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p>Percent:</p>
+      <input name="percent" placeholder="Enter persent" ref={register({ required: true, maxLength: 3 })}/>
+      {errors.percent && <span>Enter Percent 1-100</span>} 
+      <input type="submit" />
+    </form>
     </>
   );
 }
